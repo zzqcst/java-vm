8a157b37193751a8cf2bf4bbb0cabb6079b4beac
zzq <zzqlrr@gmail.com>
2018-07-22 20:27:22 +0800

    java堆内存溢出异常

--- src/main/java/com/zhi/App.java
+++ src/main/java/com/zhi/App.java
@@ -1,13 +1,24 @@
 package com.zhi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    /**
+     * java堆内存溢出异常测试
+     */
+    static class OOMObject{
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
     }
 }
