dbd1e585eaf5993c0669db0f153c81e33cdac1f5
zhi <zzqlrr@gmail.com>
2018-09-27 02:31:48 +0800

    sync

--- src/main/java/com/zhi/Test.java
+++ src/main/java/com/zhi/Test.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         String[] words = {"i","heard","about","spark","and","i","love","spark"};
-        System.out.println(words[0].hashCode());
+        System.out.println(words[0].hashCode()%2000);
     }
 }
