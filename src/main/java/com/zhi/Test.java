cfc2c528472e3ef307cd41e68bb49f69d375d548
zhi <zzqlrr@gmail.com>
2018-09-27 02:20:04 +0800

    sync

---
+++ src/main/java/com/zhi/Test.java
@@ -0,0 +1,10 @@
+package com.zhi;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        String[] words = {"i","heard","about","spark","and","i","love","spark"};
+        System.out.println(words[0].hashCode());
+    }
+}
